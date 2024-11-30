ae502411034da971f0ae93e168d0f418340964cb
Robbin Marcus <robbin.marcus@gmail.com>
2024-11-30 16:35:45 +0100

    Matrices article and discord link

--- js/drawing.js
+++ js/drawing.js
@@ -96,6 +96,55 @@ class Float3
     }
 }
 
+class Matrix2
+{
+    constructor(values)
+    {
+        if (Array.isArray(values) && values.length == 4)
+        {
+            this.data = values;
+        }
+        else
+        {
+            this.data = Array(4).fill(0);
+        }
+    }
+
+    MultiplyVector(v)
+    {
+        return new Float2(
+            v.x * this.data[0] + v.y * this.data[1],
+            v.x * this.data[2] + v.y * this.data[3],
+        );
+    }
+}
+
+class Matrix4
+{
+    constructor(values)
+    {
+        if (Array.isArray(values) && values.length == 16)
+        {
+            this.data = values;
+        }
+        else
+        {
+            this.data = Array(16).fill(0);
+        }
+    }
+
+    MultiplyVector(v)
+    {
+        return new Float4(
+            v.x * this.data[0] + v.y * this.data[1] + v.z * this.data[2] + v.w * this.data[3],
+            v.x * this.data[4] + v.y * this.data[5] + v.z * this.data[6] + v.w * this.data[7],
+            v.x * this.data[8] + v.y * this.data[9] + v.z * this.data[10] + v.w * this.data[11],
+            v.x * this.data[12] + v.y * this.data[13] + v.z * this.data[14] + v.w * this.data[15],
+        );
+    }
+}
+
+
 class Ray 
 {
     constructor(pos, dir)
